419654396192eaa943e82a62f209243006bca2bc
skippergoroye <oluwatosinadegoroye21@gmail.com>
2023-05-29 20:43:31 -0700

    image viewer created

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+
+import PlaceholderImage from './assets/images/background-image.png'
+import ImageViewer from './components/ImageViewer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start welcome </Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer : {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image : {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  }
 });
